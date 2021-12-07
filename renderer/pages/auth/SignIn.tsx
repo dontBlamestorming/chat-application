20c48aeab6bc72c4e737a595cf90c9286883b88d
ukdong <lklone20005@gmail.com>
2021-12-07 14:10:44 +0900

    add SignIn && SignUp Component

---
+++ renderer/pages/auth/SignIn.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function SignIn() {
+
+  return (
+    <React.Fragment>
+      <h1>Sign In page</h1>
+    </React.Fragment>
+  )
+}
---
+++ renderer/pages/auth/SignUp.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function SignUp() {
+
+  return (
+    <React.Fragment>
+      <h1>Sign Up page</h1>
+    </React.Fragment>
+)
+}
